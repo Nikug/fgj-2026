0b1e5c99af48369ce587077178cff310580a2938
Niku Grönberg <gronbergniku@gmail.com>
2026-01-30 22:28:13 +0200

    random generation

--- skeittipeli/ground.gd
+++ skeittipeli/ground.gd
@@ -1,26 +1,36 @@
 extends Node2D
 
+@export var resolution: int = 100
+@export var segment_width: float = 10.0
+@export var segment_height: float = 20.0
+@export var noise_strength: float = 5.0
+
 @onready var collision_polygon: CollisionPolygon2D = $Polygon2D/StaticBody2D/CollisionPolygon2D
 @onready var polygon: Polygon2D = $Polygon2D
-
-var timer: float = 0.0
-var limit: float = 2
+@onready var fast_noise_lite: FastNoiseLite = FastNoiseLite.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-  pass # Replace with function body.
+  fast_noise_lite.set_seed(randi())
+  fast_noise_lite.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
+  fast_noise_lite.fractal_ping_pong_strength = 0.9
+
+  var new_segment: PackedVector2Array = PackedVector2Array()
+  new_segment.append(Vector2(0, 0))
+
+  for i in range(1, resolution):
+    var x: float = i * segment_width / resolution
+    var y: float = fast_noise_lite.get_noise_2d(i, 0) * noise_strength
+    new_segment.append(Vector2(x, y))
+
+  new_segment.append(Vector2(10, 0))
+  new_segment.append(Vector2(10, -segment_height))
+  new_segment.append(Vector2(0, -segment_height))
+
+  collision_polygon.polygon = new_segment
+  polygon.polygon = new_segment
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-  if timer > limit:
-    timer = 0
-    generate_new_polygon_shape()
-  else:
-    timer += delta
-
-func generate_new_polygon_shape() -> void:
-  var new_polygon: PackedVector2Array = PackedVector2Array()
-  for i in range(0, collision_polygon.polygon.size()):
-    new_polygon.append(collision_polygon.polygon[i] + Vector2(randf() * 100, randf() * 100))
-  collision_polygon.polygon = new_polygon
+func _process(_delta: float) -> void:
+  pass
