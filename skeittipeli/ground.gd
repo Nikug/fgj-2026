b2a8e07f102f8e4a4c8fba5d43895f2d18ee1b06
Niku Grönberg <gronbergniku@gmail.com>
2026-01-30 22:40:15 +0200

    make segments

--- skeittipeli/ground.gd
+++ skeittipeli/ground.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
 @export var resolution: int = 100
-@export var segment_width: float = 10.0
-@export var segment_height: float = 20.0
-@export var noise_strength: float = 5.0
+@export var segment_width: float = 200.0
+@export var segment_height: float = 200.0
+@export var noise_strength: float = 50.0
 
 @onready var collision_polygon: CollisionPolygon2D = $Polygon2D/StaticBody2D/CollisionPolygon2D
 @onready var polygon: Polygon2D = $Polygon2D
@@ -15,22 +15,26 @@ func _ready() -> void:
   fast_noise_lite.noise_type = FastNoiseLite.TYPE_SIMPLEX_SMOOTH
   fast_noise_lite.fractal_ping_pong_strength = 0.9
 
+  var segment = _generate_segment(Vector2(0, 0), Vector2(segment_width, 0))
+  polygon.polygon = segment
+  collision_polygon.polygon = segment
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+  pass
+
+func _generate_segment(start_point: Vector2, end_point: Vector2) -> PackedVector2Array:
   var new_segment: PackedVector2Array = PackedVector2Array()
-  new_segment.append(Vector2(0, 0))
+  new_segment.append(start_point)
 
   for i in range(1, resolution):
     var x: float = i * segment_width / resolution
     var y: float = fast_noise_lite.get_noise_2d(i, 0) * noise_strength
     new_segment.append(Vector2(x, y))
 
-  new_segment.append(Vector2(10, 0))
-  new_segment.append(Vector2(10, -segment_height))
-  new_segment.append(Vector2(0, -segment_height))
+  new_segment.append(Vector2(segment_width, end_point.y))
+  new_segment.append(Vector2(segment_width, segment_height))
+  new_segment.append(Vector2(start_point.x, segment_height))
 
-  collision_polygon.polygon = new_segment
-  polygon.polygon = new_segment
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-  pass
+  return new_segment
