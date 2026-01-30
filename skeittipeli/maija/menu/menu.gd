43da8b339f13f6abef0d738faaa800eb99c2ef7f
petterigit <p-makela@outlook.com>
2026-01-30 23:39:21 +0200

    Add cutscene intro to game

--- skeittipeli/maija/menu/menu.gd
+++ skeittipeli/maija/menu/menu.gd
@@ -11,7 +11,7 @@ func _process(delta):
 
 
 func _on_start_game_button_pressed():
-	get_tree().change_scene_to_file("res://main2.tscn")
+	get_tree().change_scene_to_file("res://vilperi/Cutscene.tscn")
 
 
 func _on_exit_game_button_pressed():
