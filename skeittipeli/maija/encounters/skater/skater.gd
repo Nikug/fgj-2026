230cca783a3b049bfdb38825d7bf47d14fe670e8
Maija <maijaaaw@gmail.com>
2026-01-30 23:19:24 +0200

    skater

--- skeittipeli/maija/encounters/skater/skater.gd
+++ skeittipeli/maija/encounters/skater/skater.gd
@@ -1,8 +1,10 @@
 extends AnimatedSprite2D
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	play("jump")
+	play("kick")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
