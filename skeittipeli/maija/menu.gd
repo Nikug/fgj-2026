c5c4f5301c6a80216608a58cab7d449d880617b8
Niku Grönberg <gronbergniku@gmail.com>
2026-01-30 22:07:20 +0200

    scenes

--- skeittipeli/maija/menu.gd
+++ skeittipeli/maija/menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_game_button_pressed():
-	get_tree().change_scene_to_file("res://ground.tscn")
+	get_tree().change_scene_to_file("res://main2.tscn")
 
 
 func _on_exit_game_button_pressed():
