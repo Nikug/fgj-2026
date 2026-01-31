50aeca45a3f5071a062be2917e8ef81e6653544f
Niku Grönberg <gronbergniku@gmail.com>
2026-01-31 13:11:49 +0200

    now it works(tm)

--- skeittipeli/ground.gd
+++ skeittipeli/ground.gd
@@ -2,9 +2,9 @@ extends Node2D
 
 @export var resolution: int = 50
 @export var segment_width: float = 200.0
-@export var segment_height: float = 200.0
+@export var segment_height: float = 500.0
 @export var noise_strength: float = 50.0
-@export var decline_rate: float = 5.0
+@export var decline_rate: float = 2.0
 
 @onready var collision_polygon: CollisionPolygon2D = $StaticBody2D/CollisionPolygon2D
 @onready var polygon: Polygon2D = $Polygon2D
@@ -25,21 +25,27 @@ func set_segment(segment: PackedVector2Array) -> void:
 
 func generate_segment() -> Array:
 	var new_segment: PackedVector2Array = PackedVector2Array()
-	new_segment.append(Vector2.ZERO)
 
+	var decline: float = decline_rate * resolution
+	var start_height: float = decline
 	var end_height: float = 0.0
+
+	new_segment.append(Vector2(0, start_height))
+
 	for i in range(1, resolution):
 		var x: float = i * segment_width / resolution
 		var y: float = fast_noise_lite.get_noise_2d(x, 0) * noise_strength
-		var decline: float = i * segment_height / resolution * decline_rate
+		decline -= decline_rate
 		end_height = y + decline
 		new_segment.append(Vector2(x, end_height))
 
+	# Last point in the generated line
 	new_segment.append(Vector2(segment_width, end_height))
-	new_segment.append(Vector2(segment_width, end_height + segment_height))
-	new_segment.append(Vector2(0, end_height + segment_height))
+	# Points to create bottom of the shape
+	new_segment.append(Vector2(segment_width, segment_height))
+	new_segment.append(Vector2(0, segment_height + start_height))
 
-	return [new_segment, end_height]
+	return [new_segment, start_height, end_height]
 
 
 func get_width() -> float:
