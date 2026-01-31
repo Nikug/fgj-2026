838c962dad6f8c57ac0ac0a599edf0a9a8817b4a
Apezkin <51073470+Apezkin@users.noreply.github.com>
2026-01-31 23:53:06 +0200

    jump cooldown

--- skeittipeli/aleksi/ukkeli.gd
+++ skeittipeli/aleksi/ukkeli.gd
@@ -41,6 +41,9 @@ var air_speed_limit: float = 80
 var rotation_limit: float = 300
 var max_movement_speed: float = 2000
 var current_movement_speed: float = 0.0
+var default_jump_cooldown: float = 1.0
+
+var rng = RandomNumberGenerator.new()
 
 
 func _ready():
@@ -51,6 +54,7 @@ func _ready():
     plague_mask_sprite.visible = true
     hockey_mask_sprite.visible = false
     ghost_mask_sprite.visible = false
+    $JumpCooldown.wait_time = rng.randf_range(1.0, 3.0)
   elif mask.selectedMask == 1:
     print("hockey")
     ground_accel = 100
@@ -65,6 +69,7 @@ func _ready():
     plague_mask_sprite.visible = false
     hockey_mask_sprite.visible = true
     ghost_mask_sprite.visible = false
+    $JumpCooldown.wait_time = 1
   elif mask.selectedMask == 2:
     print("ghost")
     ground_accel = 10
@@ -79,6 +84,7 @@ func _ready():
     plague_mask_sprite.visible = false
     hockey_mask_sprite.visible = false
     ghost_mask_sprite.visible = true
+    $JumpCooldown.wait_time = 3.0
 
 
 func _physics_process(delta: float):
@@ -125,7 +131,8 @@ func _physics_process(delta: float):
       var slide_direction := get_last_slide_collision().get_normal()
       velocity = velocity.slide(slide_direction)
     # Handle Jump.
-  if Input.is_action_just_pressed("jump"):
+  if Input.is_action_just_pressed("jump") and $JumpCooldown.is_stopped():
+    $JumpCooldown.start()
     if collided:
           velocity += get_last_slide_collision().get_normal().rotated(deg_to_rad(-90)) * 200
           play_jump_animation()
@@ -135,6 +142,8 @@ func _physics_process(delta: float):
         velocity += get_wall_normal().rotated(-0.75 * PI) * 400
     else:
         velocity.x -= 600
+
+  print($JumpCooldown.is_stopped())
 
   # Always apply minimum leftward velocity
   velocity.x = min(min_left_velocity, max(-max_movement_speed, velocity.x))
@@ -193,3 +202,7 @@ func play_jump_animation():
     plague_mask_sprite.play("jump")
     hockey_mask_sprite.play("jump")
     ghost_mask_sprite.play("jump")
+
+
+func _on_jump_cooldown_timeout():
+    $JumpCooldown.stop()
