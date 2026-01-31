dd353347ff5106b645e7fb85534b54b4dcfc23a8
petterigit <p-makela@outlook.com>
2026-02-01 00:38:52 +0200

    mask situation

--- skeittipeli/vilperi/select_mask.gd
+++ skeittipeli/vilperi/select_mask.gd
@@ -9,13 +9,21 @@ extends Node;
 @export var nextButton: Button;
 @export var startButton: Button;
 
+
+# X axis camera movement
 @export var camera_speed: float = 50.0
 @export var camera_acceleration: float = 10.0
 @export var camera_max_speed: float = 300.0
+# Y axis camera movement (slower)
+@export var camera_y_speed: float = 10.0
+@export var camera_y_acceleration: float = 2.0
+@export var camera_y_max_speed: float = 40.0
 
 var camera: Camera3D
-var camera_velocity: float = 0.0
+var camera_velocity_x: float = 0.0
+var camera_velocity_y: float = 0.0
 var target_x: float = 0.0
+var target_y: float = 0.0
 var is_moving_camera: bool = false
 
 func _ready() -> void:
@@ -24,20 +32,21 @@ func _ready() -> void:
         $"../Mask 2",
         $"../Mask 3"
     ]
-    
+
     spotlights = [
         $"../SpotlightMask1",
         $"../SpotlightMask2",
         $"../SpotlightMask3"
     ]
-    
+
     camera = get_viewport().get_camera_3d()
     target_x = camera.global_position.x
-    
+    target_y = camera.global_position.y
+
     # Connect buttons
     prevButton = $PrevButton
     prevButton.pressed.connect(_on_prev_pressed)
-    
+
     nextButton = $NextButton
     nextButton.pressed.connect(_on_next_pressed)
 
@@ -60,22 +69,38 @@ func _physics_process(delta: float) -> void:
 
 func update_camera_movement(delta: float) -> void:
     var current_x = camera.global_position.x
-    var distance = target_x - current_x
-    
-    # Accelerate towards target
-    if abs(distance) > 0.1: # Stop when very close
-        if distance > 0:
-            camera_velocity = min(camera_velocity + camera_acceleration * delta, camera_max_speed)
+    var current_y = camera.global_position.y
+    var distance_x = target_x - current_x
+    var distance_y = target_y - current_y
+
+    # Accelerate towards target X
+    if abs(distance_x) > 0.1:
+        if distance_x > 0:
+            camera_velocity_x = min(camera_velocity_x + camera_acceleration * delta, camera_max_speed)
         else:
-            camera_velocity = max(camera_velocity - camera_acceleration * delta, -camera_max_speed)
-        
-        current_x += camera_velocity * delta
+            camera_velocity_x = max(camera_velocity_x - camera_acceleration * delta, -camera_max_speed)
+        current_x += camera_velocity_x * delta
     else:
         current_x = target_x
-        camera_velocity = 0.0
+        camera_velocity_x = 0.0
+
+    # Accelerate towards target Y (slower)
+    if abs(distance_y) > 0.1:
+        if distance_y > 0:
+            camera_velocity_y = min(camera_velocity_y + camera_y_acceleration * delta, camera_y_max_speed)
+        else:
+            camera_velocity_y = max(camera_velocity_y - camera_y_acceleration * delta, -camera_y_max_speed)
+        current_y += camera_velocity_y * delta
+    else:
+        current_y = target_y
+        camera_velocity_y = 0.0
+
+    # If both X and Y are close enough, stop moving
+    if abs(distance_x) <= 0.1 and abs(distance_y) <= 0.1:
         is_moving_camera = false
-    
+
     camera.global_position.x = current_x
+    camera.global_position.y = current_y
 
 func _on_prev_pressed() -> void:
     current_mask_index = (current_mask_index - 1 + masks.size()) % masks.size()
@@ -91,8 +116,10 @@ func _on_start_pressed() -> void:
 
 func update_mask_display() -> void:
     target_x = masks[current_mask_index].global_position.x
+    target_y = masks[current_mask_index].global_position.y + 2
     is_moving_camera = true
-    camera_velocity = 0.0
+    camera_velocity_x = 0.0
+    camera_velocity_y = 0.0
     update_button_visibility()
     update_spotlight_visibility()
 
