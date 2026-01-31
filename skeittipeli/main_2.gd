75c8a6f59c3e4224aa65250d853235fcd6266a5a
Apezkin <51073470+Apezkin@users.noreply.github.com>
2026-01-31 12:18:44 +0200

    move texture rect

--- skeittipeli/main_2.gd
+++ skeittipeli/main_2.gd
@@ -3,19 +3,19 @@ var score
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    new_game()
+	new_game()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass
 
 func new_game():
-    score = 0
-    $Hud.update_score(score)
-    $Hud.show_message("Time to skate")
-    $ScoreTimer.start()
+	score = 0
+	$Hud.update_score(score)
+	$Hud.show_message("Time to skate")
+	$ScoreTimer.start()
 
 func _on_score_timer_timeout():
-    score += 1
-    $Hud.update_score(score)
+	score += 1
+	$Hud.update_score(score)
