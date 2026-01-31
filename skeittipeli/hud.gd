e1be53f2c95dc1a1fb7562150e80af9faa4417cf
Apezkin <51073470+Apezkin@users.noreply.github.com>
2026-01-31 16:09:04 +0200

    for all indents and purposes

--- skeittipeli/hud.gd
+++ skeittipeli/hud.gd
@@ -3,32 +3,32 @@ extends CanvasLayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+    pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_just_pressed("ui_cancel"):
-		get_tree().change_scene_to_file("res://main.tscn")
+    if Input.is_action_just_pressed("ui_cancel"):
+        get_tree().change_scene_to_file("res://main.tscn")
 
 
 func show_message(text):
-	$Message.text = text
-	$Message.show()
-	$MessageTimer.start()
+    $Message.text = text
+    $Message.show()
+    $MessageTimer.start()
 
 func _on_message_timer_timeout():
-	$Message.hide()
+    $Message.hide()
 
 func show_game_over():
-	show_message("Game Over")
-	await $MessageTimer.timeout
+    show_message("Game Over")
+    await $MessageTimer.timeout
 
-	get_tree().change_scene_to_file("res://main.tscn")
+    get_tree().change_scene_to_file("res://main.tscn")
 
 func update_score(score):
-	$ScoreLabel.text = str(score)
+    $ScoreLabel.text = str(score)
 
 
 func _on_button_pressed():
-	get_tree().change_scene_to_file("res://main.tscn")
+    get_tree().change_scene_to_file("res://main.tscn")
