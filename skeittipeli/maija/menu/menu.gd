aa7bb4191be26f469819d7895d40e44ce2c1a789
Apezkin <51073470+Apezkin@users.noreply.github.com>
2026-01-31 12:44:35 +0200

    skip cutscene, I am busy

--- skeittipeli/maija/menu/menu.gd
+++ skeittipeli/maija/menu/menu.gd
@@ -7,7 +7,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("enter"):
+		get_tree().change_scene_to_file("res://main2.tscn")
 
 
 func _on_start_game_button_pressed():
