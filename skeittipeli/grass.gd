7f66fa73ce6968923b926cd54aa359862ff07020
Niku Grönberg <gronbergniku@gmail.com>
2026-01-31 23:53:38 +0200

    ass to grass

--- skeittipeli/grass.gd
+++ skeittipeli/grass.gd
@@ -2,8 +2,8 @@ extends Node2D
 
 @onready var line: Line2D = $Line2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-  var polygon: Polygon2D = get_parent().get_node("Polygon2D")
-  if polygon.polygon.size() > 0:
-    line.points = polygon.polygon.slice(0, polygon.polygon.size() - 3)
+func draw_grass(points: PackedVector2Array):
+  var moved_points = PackedVector2Array()
+  for i in range(0, points.size()):
+    moved_points.append(points[i] + Vector2(0, 16))
+  line.points = moved_points
