fb56bda600ef6edb3835786305c7197111c6057c
Niku Grönberg <gronbergniku@gmail.com>
2026-02-01 14:18:33 +0200

    action camera

--- skeittipeli/main_camera.gd
+++ skeittipeli/main_camera.gd
@@ -10,5 +10,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
   var player = get_node("/root/Main2/Pelaaja")
-  position = lerp(position, player.position, follow_speed * delta)
-  pass
+  if player != null:
+    position = lerp(position, player.position, follow_speed * delta)
+    var velocity: Vector2 = player.velocity
+    var speed: float = velocity.length()
+    var zoom_level = 1 / lerp(0.9, 1.2, speed / 1000)
+    zoom = lerp(zoom, Vector2(zoom_level, zoom_level), delta * 2)
