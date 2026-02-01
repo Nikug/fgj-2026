1612afadd680d806d0c28606917c98dabe8ace56
Niku Grönberg <gronbergniku@gmail.com>
2026-02-01 12:10:33 +0200

    fix ghosts n stuff

--- skeittipeli/main_2.gd
+++ skeittipeli/main_2.gd
@@ -8,7 +8,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    if Input.is_action_just_pressed("r"):
+      if Input.is_action_just_pressed("r"):
+        $Pelaaja.jump_cooldown.stop()
         $DeathTimer.stop()
         $Pelaaja.cancelDie()
         $Hud.cancel_game_over()
